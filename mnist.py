53075d236a8c98c0251db58d70bc415a1478e122
carlfeynman <coolarun477@gmail.com>
2023-09-13 09:35:01 +0530

    ipynb to py conversion process added

--- mnist.py
+++ mnist.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
-
-
 import torch
 from torch import nn
 import torch.nn.functional as F
@@ -19,15 +16,9 @@ get_ipython().run_line_magic('matplotlib', 'inline')
 plt.rcParams['figure.figsize'] = [2, 2]
 
 
-# In[2]:
-
-
 dataset_nm = 'mnist'
 x,y = 'image', 'label'
 ds = load_dataset(dataset_nm)
-
-
-# In[3]:
 
 
 def transform_ds(b):
@@ -36,9 +27,6 @@ def transform_ds(b):
 
 dst = ds.with_transform(transform_ds)
 plt.imshow(dst['train'][0]['image'].permute(1,2,0));
-
-
-# In[4]:
 
 
 bs = 1024
@@ -56,9 +44,6 @@ xb,yb = next(iter(dls.train))
 xb.shape, yb.shape
 
 
-# In[5]:
-
-
 class Reshape(nn.Module):
     def __init__(self, dim):
         super().__init__()
@@ -66,9 +51,6 @@ class Reshape(nn.Module):
     
     def forward(self, x):
         return x.reshape(self.dim)
-
-
-# In[6]:
 
 
 def cnn_classifier():
@@ -89,9 +71,7 @@ def cnn_classifier():
     )
 
 
-# In[7]:
-
-
+# model definition
 def linear_classifier():
     return nn.Sequential(
         Reshape((-1, 784)),
@@ -101,9 +81,6 @@ def linear_classifier():
         nn.ReLU(),
         nn.Linear(50, 10)
     )
-
-
-# In[8]:
 
 
 model = linear_classifier()
@@ -133,128 +110,7 @@ for epoch in range(epochs):
     
 
 
-# In[31]:
 
-
-# with open('./linear_classifier.pkl', 'wb') as model_file:
-#     pickle.dump(model, model_file)
-
-
-# #### commit to .py file for deployment
-
-# In[32]:
-
-
-get_ipython().system('jupyter nbconvert --to script mnist.ipynb')
-
-
-# In[29]:
-
-
-get_ipython().system('pip3 install markupsafe')
-
-
-
-# In[ ]:
-
-
-# from IPython.display import HTML, display, Image
-# %%html
-# <style>
-#     #whiteboard {
-#         border: 3px solid black;
-#         border-radius: 6px;  
-#         background-color: #FFFFFF;
-#     }
-#     #capture-button {
-#         background-color: #3F52D9; 
-#         color: white;
-#         border: none;
-#         padding: 10px 20px;
-#         cursor: pointer;
-#         font-size: 16px;
-#         border-radius: 3px;
-#         margin-top: 10px;
-#         width: 190px;
-#         margin-right: 20px;
-#     }
-#     #clear-button {
-#         background-color: #FF0000,; 
-#         color: black;
-#         border: none;
-#         padding: 10px 20px;
-#         cursor: pointer;
-#         font-size: 16px;
-#         border-radius: 3px;
-#         margin-top: 10px;
-#         width: 190px;
-#     }
-#     #container {
-#       display: flex;
-#       flex-direction: column; /* Arrange children vertically */
-#       align-items: center; /* Center horizontally */
-#       justify-content: center;
-#     }
-#     #btn-container {
-#       display: flex;
-#       flex-direction: row; /* Arrange children vertically */
-#       align-items: center; /* Center horizontally */
-#     }
-
-# </style>
-# <div id='container'>
-# <canvas id="whiteboard" width="400" height="200" fill_rect='white'></canvas>
-# <div id='btn-container'>
-# <button id="capture-button">Predict</button>
-# <button id="clear-button">Clear</button>
-# </div>
-
-# </div>
-# <script>
-#     var canvas = document.getElementById('whiteboard');
-#     var context = canvas.getContext('2d');
-#     var drawing = false;
-#     canvas.addEventListener('mousedown', function (e) {
-#         drawing = true;
-#         context.beginPath();
-#         context.moveTo(e.clientX - canvas.getBoundingClientRect().left, e.clientY - canvas.getBoundingClientRect().top);
-#     });
-#     canvas.addEventListener('mousemove', function (e) {
-#         if (drawing) {
-#             context.lineTo(e.clientX - canvas.getBoundingClientRect().left, e.clientY - canvas.getBoundingClientRect().top);
-#             context.stroke();
-#         }
-#     });
-#     canvas.addEventListener('mouseup', function () {
-#         drawing = false;
-#     });
-#     canvas.addEventListener('mouseout', function () {
-#         drawing = false;
-#     });
-    
-#     var clearButton = document.getElementById('clear-button');
-#     clearButton.addEventListener('click', function () {
-#        context.clearRect(0, 0, canvas.width, canvas.height);
-#     });
-
-#    var captureButton = document.getElementById('capture-button');
-#     captureButton.addEventListener('click', function () {
-#        // Convert the canvas content to a data URL (image)
-#     var imageData = canvas.toDataURL("image/png");
-
-#     // Send the image data to the Jupyter kernel variable
-#     IPython.notebook.kernel.execute('image_data = "' + imageData + '"');
-#     });
-# </script>
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
 
 
 
