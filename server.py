1ab4c61b4e2c0bd431235c9d8dc52fdfa77a97a3
carlfeynman <coolarun477@gmail.com>
2023-09-14 13:33:54 +0530

    static folder added

--- server.py
+++ server.py
@@ -1,6 +1,14 @@
 from fastapi import FastAPI
+from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
+from pathlib import Path
 
 app = FastAPI()
+
+app.mount("/static", StaticFiles(directory=Path("static")), name="static")
+@app.get("/")
+async def root():
+    return FileResponse("static/index.html")
 
 @app.get("/predict")
 async def predict():
